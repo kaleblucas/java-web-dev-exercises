2768a4a32a608e1e1a882ea02973b2211373499c
Kaleb Lucas <klucas765@gmail.com>
2020-01-06 20:30:48 -0600

    Completed Studio 1, partial bonus

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,18 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter a radius:");
+        String radiusInput = input.nextLine();
+        while (Validation.validateRadius(radiusInput) == false) {
+            System.out.println(radiusInput + " is not a valid radius.");
+            System.out.println("Please, enter a valid radius:");
+            radiusInput = input.nextLine();
+        }
+        double radius = Double.parseDouble(radiusInput);
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+    }
+}
